06efcab3eb95af5970632ec03b949ba318291d28
Sahithyan <sahithyan2701@gmail.com>
2024-11-25 17:26:25 +0530

    chore: change font to DM Sans

--- astro.config.mjs
+++ astro.config.mjs
@@ -55,7 +55,7 @@ export default defineConfig({
 					tag: "link",
 					attrs: {
 						rel: "stylesheet",
-						href: "https://fonts.googleapis.com/css2?family=Product+Sans:wght@400;500;700&display=swap",
+						href: "https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&display=swap",
 					},
 				},
 			].concat(
