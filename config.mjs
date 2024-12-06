c32ba168c8c0fbfb2555e36072230187d4014f9a
Sahithyan <sahithyan2701@gmail.com>
2024-12-06 20:07:39 +0530

    chore: disable pdfs

--- config.mjs
+++ config.mjs
@@ -1,3 +1,3 @@
 export const SITE_TITLE = "Sahithyan's S1";
 export const SITE_HREF = "https://s1.sahithyan.dev";
-export const IS_NOTES_AS_PDFS_ENABLED = true;
+export const IS_NOTES_AS_PDFS_ENABLED = false;
