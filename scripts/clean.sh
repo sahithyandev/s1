3ad70e1b3aabf50b14384f48bc591136d76ea97a
Sahithyan <sahithyan2701@gmail.com>
2025-04-14 02:13:54 +0530

    chore: minor cleanup

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,1 +1,1 @@
-rm -rf .shell dist .astro node_modules astro.config.mjs bun.lock package.json tsconfig.json src/components utils tailwind.config.mjs
+rm -rf .shell dist .astro node_modules astro.config.mjs bun.lock package.json tsconfig.json src/components utils tailwind.config.mjs src/pages/og public/fonts plugins
