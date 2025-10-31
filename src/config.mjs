0f4c99fc1f6d218699268ae98b5432efc6ff384d
Sahithyan <sahithyan2701@gmail.com>
2025-10-31 11:47:19 +0530

    chore: remove analytics

--- src/config.mjs
+++ src/config.mjs
@@ -28,21 +28,6 @@ export const BOOK_CONFIG = {
 };
 
 /**
- * @type {Parameters<typeof starlight>[0]["head"]}
- */
-export const STARLIGHT_CONFIG_HEAD = [];
-if (process.env.NODE_ENV === "production") {
-  STARLIGHT_CONFIG_HEAD.push({
-    tag: "script",
-    attrs: {
-      defer: true,
-      "data-domain": "s1.sahithyan.dev",
-      src: "https://analytics.sahithyan.dev/js/script.js",
-    },
-  });
-}
-
-/**
  * @type {Parameters<typeof starlight>[0]["sidebar"]}
  */
 export const STARLIGHT_SIDEBAR_CONFIG = [
